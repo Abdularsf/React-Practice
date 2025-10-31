686cd6d5a14399d9066bceba2481efc668544a26
Abdul Rahman <abdularsf147@gmail.com>
2025-10-31 21:46:02 +0530

    custom hook implemented

--- src/components/hooks/customhook/customhook.jsx
+++ src/components/hooks/customhook/customhook.jsx
@@ -1,20 +1,65 @@
 import { useState, useEffect } from 'react';
-export default function StatusBar() {
-    const [isOnline, setIsOnline] = useState(true);
-    useEffect(() => {
-        function handleOnline() {
-            setIsOnline(true);
+// export default function StatusBar() {
+//     const [isOnline, setIsOnline] = useState(true);
+//     useEffect(() => {
+//         function handleOnline() {
+//             setIsOnline(true);
 
-        }
-        function handleOffline() {
-            setIsOnline(false);
+//         }
+//         function handleOffline() {
+//             setIsOnline(false);
 
-        }
-        window.addEventListener('online', handleOnline);
-        window.addEventListener('offline', handleOffline);
-        return () => {
-            window.removeEventListener('online', handleOnline); window.removeEventListener('offline', handleOffline);
-        };
-    }, []);
-    return <h1>{isOnline ? '✅ Online' : '❌ Disconnected'}</h1>;
+//         }
+//         window.addEventListener('online', handleOnline);
+//         window.addEventListener('offline', handleOffline);
+//         return () => {
+//             window.removeEventListener('online', handleOnline); window.removeEventListener('offline', handleOffline);
+//         };
+//     }, []);
+//     return <h1>{isOnline ? '✅ Online' : '❌ Disconnected'}</h1>;
+// }
+
+// using our custom hook
+
+import { useState, useEffect } from "react";
+
+export function useOnlineStatus() {
+  const [isOnline, setIsOnline] = useState(true);
+
+  useEffect(() => {
+    function handleOnline() {
+      setIsOnline(true);
+    }
+    function handleOffline() {
+      setIsOnline(false);
+    }
+
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
+
+  return isOnline; // 👈 Return the value so components can use it
+}
+
+//using that custom hook in our component
+
+import { useOnlineStatus } from "./useOnlineStatus";
+
+export default function SaveButton() {
+  const isOnline = useOnlineStatus();
+
+  function handleSaveClick() {
+    console.log("✅ Progress saved");
+  }
+
+  return (
+    <button disabled={!isOnline} onClick={handleSaveClick}>
+      {isOnline ? "Save progress" : "Reconnecting..."}
+    </button>
+  );
 }
